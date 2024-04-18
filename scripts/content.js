4d8824a5fdd5597df57f7a9daef96eaf8468a86a
Sheshant-Manure <msheshant1997@gmail.com>
2024-04-18 23:21:59 +0530

    Feat: automated toxicity check & set bgColors for each class

--- scripts/content.js
+++ scripts/content.js
@@ -4,18 +4,42 @@
     let data = {
       "statement": ""
     };
+    let nonToxicStatement;
   
     // The twitter DOM tree takes few seconds to load completely. Therefore, we wait for 7 seconds to select the tweet text
+    let typingTimer;
     setTimeout(() => {
       tweet = document.querySelector('.public-DraftStyleDefault-block');
-  
+      
       // Add listener for DOM changes (optional for efficiency)
       tweet.addEventListener("DOMSubtreeModified", () => {
         let text = tweet.textContent || tweet.innerText;
         data.statement = text;
-      });
-    }, 7000);
-  
+        if(data.statement === nonToxicStatement) {
+          tweet.style.backgroundColor = "green";
+        }
+        else if(data.statement === ""){
+          tweet.style.backgroundColor = "transparent";
+        }
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(()=>{
+            if(data.statement.length > 1 && data.statement !== nonToxicStatement){
+              checkToxicity();
+            }
+          }, 5000);
+        })
+      }, 5000);
+
+      function copyToClipboard(text) {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                console.log('Text copied to clipboard:', text);
+            })
+            .catch(err => {
+                console.error('Failed to copy text to clipboard:', err);
+            });
+    }    
+
     const checkToxicity = async () => {
       console.log('checking toxicity for: ', data.statement);
       try {
@@ -29,6 +53,15 @@
         if (response.ok) {
           const toxicity = await response.json();
           console.log(toxicity);
+          if(toxicity.class === "toxic"){
+            tweet.style.backgroundColor = "red";
+            alert("This statement contains potentially harmful language.");
+            nonToxicStatement = toxicity['non-toxic statement'];
+            copyToClipboard(nonToxicStatement);
+          }
+          else {
+            tweet.style.backgroundColor = "green";
+          }
           chrome.runtime.sendMessage({action: 'toxicityResponse', data: toxicity})
           // You can further process the toxicity data here (e.g., highlight the tweet)
         }
